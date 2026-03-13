826292b7167533fee97b212f2dd6ff1d2be14e0c
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-14 00:39:10 +0100

    task 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,8 +5,34 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+//Function
+function getInitials(names) {
+    const initials = []
+    for (let i = 0; i < names.length; i++) {
+        const thisName = names[i]
+        const thisInitial = thisName[0]
+        initials.push(thisInitial)
+    }
+    return initials
+}
+
+//Arrow Function
+
+/*
+const getInitials = names => {
+    const initials = []
+    for (let i = 0; i < names.length; i++) {
+        const thisName = names[i]
+        const thisInitial = thisName[0]
+        initials.push(thisInitial)
+    } 
+    return initials
+}
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
+const firstInitial = getInitials(names)
+console.log(firstInitial);
 
 
 
