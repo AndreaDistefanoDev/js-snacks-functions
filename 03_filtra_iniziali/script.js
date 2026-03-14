ca167b39951c87fa74516ff2a98349cf7ef04012
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-03-14 01:20:27 +0100

    task 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,8 +5,34 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+//Function
+function filterByChar(list, character) {
+    const finalArray = []
+    for (let i = 0; i < list.length; i++) {
+        const listNames = list[i]
+        const initialChar = listNames[0]
+        if (initialChar.toUpperCase() === character.toUpperCase())
+            finalArray.push(listNames)
+        
+    }
+    return finalArray
+}
 
+//Arrow Function
+/*function filterByChar(list, character) {
+    const finalArray = []
+    for (let i = 0; i < list.length; i++) {
+        const listNames = list[i]
+        const initialChar = listNames[0]
+        if (initialChar.toUpperCase() === character.toUpperCase())
+            finalArray.push(listNames)
+        
+    }
+    return finalArray
+}*/
 // Invoca la funzione qui e stampa il risultato in console
+const finalArray= filterByChar(names,'A')
+console.log(finalArray);
 
 
 
